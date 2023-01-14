17868bdd916a49c0f8b431a49b957c11c6758e59
Hitbokx <kaanchan.tiwary@gmail.com>
2023-01-14 20:57:59 +0530

    Operator overloading, just #include and use PRNG{}(min, max) to genarate any random number.

--- PRNG.h
+++ PRNG.h
@@ -1,14 +1,18 @@
 #pragma once // just #include and use PRNG{}(min, max)
 #include <random>
+#include <chrono>
 
 class PRNG
 {
+private:
+	int64_t seedTime{ std::chrono::steady_clock::now( ).time_since_epoch( ).count() };
+	int64_t seedRand{ std::random_device{}() };
 public:
 	PRNG( ) = default;
 
 	const int& operator()( int min, int max )
 	{
-		std::mt19937 mt{ std::random_device{}() };
+		std::mt19937 mt{ seedRand };
 		std::uniform_int_distribution rNum{ min,max };
 		return rNum( mt );
 	}
